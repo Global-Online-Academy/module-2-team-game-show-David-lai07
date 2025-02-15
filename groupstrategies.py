1fb1fc57727fa6c7041fa29bfb52c6ee647506ac
David Lai <david@Davids-Macbook-Air-4.local>
2025-02-14 18:08:30 -0800

    Ryan's code is really well done

--- groupstrategies.py
+++ groupstrategies.py
@@ -43,4 +43,55 @@ print (strategy2(hist4))
 
 # Your team's 3rd strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
-# 
+#here is the code, I will record video in a sec: # Your team's 3rd strategy (leave blank if you are not the driver)
+# Explanation of Strategy:
+# This strategy is about a game called "21 points". 
+# The main strategy is to win the game by getting as close to 21 points as possible but not over 21.
+# The player or dealer wins if the player or dealer's points are closer to 21 points than the other player or dealer's points.
+# Card game: 21 points
+import random
+def Cyan34Strategy3():
+    player_point = random.randint(1,10)
+    dealer_point = random.randint(1,10)
+    player_turn = True
+    if_bust = False
+    while player_point < 21 and dealer_point < 21:
+        if player_turn:
+            print("Current Player's point: ", player_point)
+            print("Current Dealer's point: ", dealer_point)
+            print("Player's turn")
+            if_want = input("Do you want another card? (y/n)")
+            if if_want == "y":
+                player_point += random.randint(1,10)
+                if player_point > 21:
+                    print("Player busts")
+                    if_bust = True
+                    break
+            else:
+                player_turn = False
+        else:
+            print("Dealer's turn")
+            if dealer_point < 17:
+                point = random.randint(1,10)
+                print("Dealer's new card point: ", point)
+                dealer_point += point
+                if dealer_point > 21:
+                    print("Dealer busts")
+                    if_bust = True
+                    break
+            else:
+                break
+    if if_bust:
+        if player_point > 21:
+            print("Dealer wins")
+        else:
+            print("Player wins")
+        return
+    else:
+        print("Final Player's point: ", player_point)
+        print("Final Dealer's point: ", dealer_point)
+        if player_point > dealer_point:
+            print("Player wins")
+        else:
+            print("Dealer wins")
+Cyan34Strategy3()
